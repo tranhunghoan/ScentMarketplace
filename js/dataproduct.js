950c40ba50e54c05295c9ab40df863aee282a176
TruongCaoPhuc <phuctruong.310103@gmail.com>
2023-11-19 23:06:00 +0700

    create page

---
+++ js/dataproduct.js
@@ -0,0 +1,394 @@
+ export var infoPerfume=[
+    {
+        id:1,
+        image:'img/perfume1.jpg',
+        sex:'female',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'chanel',
+        origin:'Ý',
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`  
+    },
+    {
+        id:2,
+        image:'img/perfume1.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'Chloe',
+        origin:'Ý',
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:3,
+        image:'img/perfume1.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'chanel',
+        origin:'Ý',
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:4,
+        image:'img/perfume1.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'chanel',
+        origin:'Ý',
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:5,
+        image:'img/perfume1.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'chanel',
+        origin:'Ý',
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:6,
+        image:'img/perfume2.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'chloe',
+        origin:'Ý',        
+        name:'Nước hoa Atelier Versace Vanille Rouge EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:7,
+        image:'img/perfume2.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'chloe',
+        origin:'Ý',        
+        name:'Nước hoa Atelier Versace Vanille Rouge EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:8,
+        image:'img/perfume2.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'chloe',
+        origin:'Ý',        
+        name:'Nước hoa Atelier Versace Vanille Rouge EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:9,
+        image:'img/perfume2.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'chloe',
+        origin:'Ý',        
+        name:'Nước hoa Atelier Versace Vanille Rouge EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:10,
+        image:'img/perfume2.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'chloe',
+        origin:'Ý',        
+        name:'Nước hoa Atelier Versace Vanille Rouge EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:11,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'Nước hoa Atelier Versace Vanille Rouge EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:12,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA PARFUMS DE MARLY PEGASUS',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:13,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA PARFUMS DE MARLY PEGASUS',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:14,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA PARFUMS DE MARLY PEGASUS',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:15,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Floral',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA PARFUMS DE MARLY PEGASUS',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:16,
+        image:'img/perfume4.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'Marc Jacobs',
+        origin:'Ý',
+        name:'NƯỚC HOA PARFUMS DE MARLY PEGASUS',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:17,
+        image:'img/perfume5.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'D&G',  
+        origin:'Ý',      
+        name:'NƯỚC HOA PARFUMS DE MARLY PEGASUS',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:18,
+        image:'img/perfume5.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'D&G',
+        origin:'Ý',      
+        name:'NƯỚC HOA PARFUMS DE MARLY PEGASUS',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:19,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA PARFUMS DE MARLY PEGASUS',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:20,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',                      
+        name:'NƯỚC HOA PARFUMS DE MARLY PEGASUS',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:21,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:22,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:23,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:24,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:25,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`  
+    },
+    {
+        id:26,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP' ,
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`   
+    },
+    {
+        id:27,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+    {
+        id:28,
+        image:'img/perfume3.jpg',
+        sex:'male',
+        concentration:'Eau de parfum',
+        incense:'Aromatic',
+        price:'500000',
+        brand:'gucci',
+        origin:'Ý',        
+        name:'NƯỚC HOA CREED SPRING FLOWER EDP',
+        description:`Hương đầu: Bạch đậu khấu, Cây vòi voi, Tiêu hồng và Cam Bergamot<br>
+                     Hương giữa: Hạnh nhân đắng, Oải hương, Hoa nhài và Phong lữ<br>
+                     Hương cuối: Vani, Gỗ Guaiac, Gỗ trầm hương (Oud), Gỗ đàn hương và Hổ phách.`    
+    },
+]
