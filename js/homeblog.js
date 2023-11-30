b7499de7f0334c24acf5bd06eadc55adf6ccc975
CamVan16 <21521665@gm.uit.edu.vn>
2023-11-30 09:20:23 +0700

    update_1

--- js/homeblog.js
+++ js/homeblog.js
@@ -1,7 +1,7 @@
 import { infoBlog } from "./datablog.js"
 var contentPage='';
 var currentPage=1;
-var numberPage=Math.ceil(infoBlog.length/3);
+var numberPage=Math.ceil(infoBlog.length/4);
 
  function renderBlog(blog){ 
     return `
@@ -10,7 +10,7 @@ var numberPage=Math.ceil(infoBlog.length/3);
     <a href="./detailblog.html?id=${blog.id}" class="img"><img src="${blog.image}" alt=""></a>
     <div class="info">
     <p class="meta">${blog.date}</p>
-    <h4 class="title-blog"><a class="directPage" href="./detailblog.html?id=${blog.id}">${blog.title}</a></h4>
+    <h4 class="title"><a class="leadToDetail" href="./detailblog.html?id=${blog.id}">${blog.title}</a></h4>
     </div>
     </div>
     </li>`
@@ -18,17 +18,16 @@ var numberPage=Math.ceil(infoBlog.length/3);
  function renderPage(currentPage,blogs)
  {
     contentPage='';
-    var firstBlog=(currentPage-1)*3;
+    var firstBlog=(currentPage-1)*4;
     var lastBlog;
-    if(currentPage<numberPage)lastBlog=currentPage*3;
+    if(currentPage<numberPage)lastBlog=currentPage*4;
     else lastBlog=blogs.length;
     for(var index=firstBlog; index<lastBlog; index++){
             contentPage+=renderBlog(blogs[index]) 
         }
     document.querySelector('.blog-list-ul').innerHTML=contentPage
 }
-function handleNextPage(blogs){
-    //document.querySelector('.direction').onclick=handleAddItem
+function handlePage(blogs){
     var buttonPages=document.querySelectorAll('button.page')
     for(let i=0;i<buttonPages.length;i++){
     buttonPages[i].onclick=function (){
@@ -36,9 +35,17 @@ function handleNextPage(blogs){
 }
 }
 }
-function start(){
-    
-    renderPage(1,infoBlog)
-    handleNextPage(infoBlog)
+function handleNextPage(blogs){
+    var buttonPages=document.querySelectorAll('button.nextPage')
+    for(let i=0;i<buttonPages.length;i++){
+    buttonPages[i].onclick=function (){
+    renderPage(2,blogs)
+}
+}
+}
+function start(){ 
+    renderPage(1,infoBlog);
+    handlePage(infoBlog);
+    handleNextPage(infoBlog);
 }
 start()
