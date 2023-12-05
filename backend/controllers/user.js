8ebd5a015efbc82aa302ae9bef5e13a9311cf73f
tranhunghoan <kamisamano17@gmail.com>
2023-12-05 15:24:06 +0700

    update middleware and helper

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,12 @@
-const getUsers = (req,res ) => {
-    return res.send('user controller')
-}
+import * as services from "../services";
+import { internalServerError, badRequest } from "../middleware/handle_error";
 
-module.exports = {
-    getUsers
-}
+export const getCurrent = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const response = await services.getOne(id);
+    return res.status(200).json(response);
+  } catch (error) {
+    return internalServerError(res);
+  }
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,12 @@
 import * as controllers from "../controllers"
 import  express from "express";
+import verifyToken from "../middleware/verify_token";
 
 const router = express.Router();
-router.get('/',controllers.getUsers);
+
+
+//Auth route
+router.use(verifyToken)
+router.get('/',controllers.getCurrent);
 
 module.exports = router;
